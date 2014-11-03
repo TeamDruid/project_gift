d01a6ec3d7908cea3c7fd5268cdc49be349e60d6
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-11-03 16:24:53 -0500

    Creating fragment_zodiac xml file

--- app/src/main/java/com/detroitlabs/teamdruid/project_gift/Fragments/Zodiac.java
+++ app/src/main/java/com/detroitlabs/teamdruid/project_gift/Fragments/Zodiac.java
@@ -1,7 +1,18 @@
 package com.detroitlabs.teamdruid.project_gift.Fragments;
 
+import android.app.Fragment;
+
 /**
  * Created by admin on 11/3/14.
  */
-public class Zodiac {
+public class Zodiac extends Fragment {
+
+    //I have no idea what to put in here.
+
+    //That's not really true.
+
+    //Zodiac needs to have its own layout of some sort,
+    //but the code itself should include
+    //-a way to search the user's birthday
+    //-a way to match the birthday to a particular zodiac sign
 }
