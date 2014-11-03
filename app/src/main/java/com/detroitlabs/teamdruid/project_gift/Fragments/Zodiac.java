d601ac7894a0ff98fb0be055421a373aa9636dc3
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-11-03 16:48:14 -0500

    Overriding onCreateView method in Zodiac fragment

--- app/src/main/java/com/detroitlabs/teamdruid/project_gift/Fragments/Zodiac.java
+++ app/src/main/java/com/detroitlabs/teamdruid/project_gift/Fragments/Zodiac.java
@@ -1,6 +1,12 @@
 package com.detroitlabs.teamdruid.project_gift.Fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.detroitlabs.teamdruid.project_gift.R;
 
 /**
  * Created by admin on 11/3/14.
@@ -15,4 +21,11 @@ public class Zodiac extends Fragment {
     //but the code itself should include
     //-a way to search the user's birthday
     //-a way to match the birthday to a particular zodiac sign
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_zodiac, container, false);
+        return rootView;
+    }
 }
