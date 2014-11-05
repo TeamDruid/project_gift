a15cab8fb5cfa23a22d08b4ce1d15172b417ac0b
acdevine89 <annie.devine@detroitlabs.com>
2014-11-05 12:31:01 -0500

    Adding search button and three edit text fields in Interests.java

--- app/src/main/java/com/detroitlabs/teamdruid/project_gift/Fragments/Interests.java
+++ app/src/main/java/com/detroitlabs/teamdruid/project_gift/Fragments/Interests.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.detroitlabs.teamdruid.project_gift.R;
 
@@ -14,10 +16,16 @@ import com.detroitlabs.teamdruid.project_gift.R;
 public class Interests extends Fragment implements View.OnClickListener {
 
 
+    Button searchInterestsButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_interests, container, false);
+
+        EditText1 = (EditText) rootView.findViewById(R.id.interests1);
+        EditText2 = (EditText) rootView.findViewById(R.id.interests2);
+        EditText3 = (EditText) rootView.findViewById(R.id.interests3);
 
     }
 
