41d27b0fa4119f00e0a47e5cc88467e5d03b9c12
elyselturner <elyse.turner@detroitlabs.com>
2014-11-05 12:03:37 -0500

    starting on our JSON stuff

---
+++ app/src/main/java/com/detroitlabs/teamdruid/project_gift/JSON/JsonData.java
@@ -0,0 +1,15 @@
+package com.detroitlabs.teamdruid.project_gift.JSON;
+
+/**
+ * Created by elyseturner on 11/5/14.
+ */
+public class JsonData {
+  private String mSearchResults = "";
+
+    public void setSearchResults(String result){
+      mSearchResults = result;
+  }
+
+    public
+
+}
---
+++ app/src/main/java/com/detroitlabs/teamdruid/project_gift/Models/EtsyObjects.java
@@ -0,0 +1,9 @@
+package com.detroitlabs.teamdruid.project_gift.Models;
+
+/**
+ * Created by elyseturner on 11/5/14.
+ */
+public class EtsyObjects {
+
+
+}
