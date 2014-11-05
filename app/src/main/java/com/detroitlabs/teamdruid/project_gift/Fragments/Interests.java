74df46ad41a7811e8c07bbf9fdd1a596c3133a8d
acdevine89 <annie.devine@detroitlabs.com>
2014-11-05 12:21:39 -0500

    Adding onCreateView with inflater, container and bundle for Interests.java, and fragment_interests.xml file

--- app/src/main/java/com/detroitlabs/teamdruid/project_gift/Fragments/Interests.java
+++ app/src/main/java/com/detroitlabs/teamdruid/project_gift/Fragments/Interests.java
@@ -1,9 +1,24 @@
 package com.detroitlabs.teamdruid.project_gift.Fragments;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.detroitlabs.teamdruid.project_gift.R;
+
 /**
  * Created by kyleofori on 11/3/14.
  */
-public class Interests {
+public class Interests extends Fragment implements View.OnClickListener {
 
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_interests, container, false);
+
+    }
 
 }
