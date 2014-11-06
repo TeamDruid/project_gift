442406005b24de977dc70fb576ffe2b9e6ce13f9
acdevine89 <annie.devine@detroitlabs.com>
2014-11-06 11:58:48 -0500

    Adding ListView, ArrayAdapter and ArrayList of Etsy objects to the SearchResults class

--- app/src/main/java/com/detroitlabs/teamdruid/project_gift/Fragments/SearchResults.java
+++ app/src/main/java/com/detroitlabs/teamdruid/project_gift/Fragments/SearchResults.java
@@ -1,9 +1,22 @@
 package com.detroitlabs.teamdruid.project_gift.Fragments;
 
+import android.app.ListFragment;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.detroitlabs.teamdruid.project_gift.Models.EtsyObjects;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by anniedevine on 11/6/14.
  */
-public class SearchResults {
+public class SearchResults extends ListFragment {
+    ListView mainListView;
+    ArrayAdapter mArrayAdapter;
+    List<EtsyObjects> mResultsList = new ArrayList<EtsyObjects>();
 
+    
 
 }
