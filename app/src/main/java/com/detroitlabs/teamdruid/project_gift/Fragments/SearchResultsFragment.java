949820b3567375c71e2efd369aee2dc4c0fba890
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-11-07 15:23:51 -0500

    Got everything working again--not everything, but on the way

--- app/src/main/java/com/detroitlabs/teamdruid/project_gift/Fragments/SearchResultsFragment.java
+++ app/src/main/java/com/detroitlabs/teamdruid/project_gift/Fragments/SearchResultsFragment.java
@@ -3,6 +3,7 @@ package com.detroitlabs.teamdruid.project_gift.Fragments;
 import android.app.ListFragment;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
