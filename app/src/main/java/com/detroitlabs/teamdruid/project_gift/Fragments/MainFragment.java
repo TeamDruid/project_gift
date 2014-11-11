4137a665101bc2d59d3805f52946acaa5df3e0b0
elyselturner <elyse.turner@detroitlabs.com>
2014-11-11 08:46:32 -0500

    I made changes to the main fragment yay

--- app/src/main/java/com/detroitlabs/teamdruid/project_gift/Fragments/MainFragment.java
+++ app/src/main/java/com/detroitlabs/teamdruid/project_gift/Fragments/MainFragment.java
@@ -18,6 +18,12 @@ import com.detroitlabs.teamdruid.project_gift.R;
  * Created by elyseturner on 11/4/14.
  */
 public class MainFragment extends Fragment {
+    // Font path
+    private final String TITLE_FONT_PATH = "fonts/LaurenScript.ttf";
+    private final String TEXT_FONT_PATH = "fonts/Walkway Bold.ttf";
+    private final String DRUID_FONT_PATH = "fonts/Walkway Bold.ttf";
+    private final String INTERESTS_FONT_PATH = "fonts/Walkway Bold.ttf";
+    private final String ZODIAC_FONT_PATH = "fonts/Walkway Bold.ttf";
 
     public static final String LOG_TAG = MainActivity.class.getSimpleName();
 
@@ -29,27 +35,25 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        // Font path
-        String titleFontPath = "fonts/LaurenScript.ttf";
-        String textFontPath = "fonts/Walkway Bold.ttf";
-        String druidFontPath = "fonts/Walkway Bold.ttf";
-        String interestsFontPath = "fonts/Walkway Bold.ttf";
-        String zodiacFontPath = "fonts/Walkway Bold.ttf";
+
         // text view label
         TextView txtGhost = (TextView) rootView.findViewById(R.id.ghost);
+
         TextView txtBoilerPlate = (TextView) rootView.findViewById(R.id.boiler_plate);
-            //KO - Changing txtDruid to a button to connect to Dan's fragment.
+
         Button txtDruid = (Button) rootView.findViewById(R.id.druid_button);
+
         TextView txtInterests = (TextView) rootView.findViewById(R.id.interests_button);
-            //KO - I changed txtZodiac to a Button so I can connect it to my fragment.
+
         Button txtZodiac= (Button) rootView.findViewById(R.id.zodiac_button);
 
         // Loading Font Face
-        Typeface tfMain = Typeface.createFromAsset(getActivity().getAssets(), titleFontPath);
-        Typeface tfText = Typeface.createFromAsset(getActivity().getAssets(), textFontPath);
-        Typeface tfDruid = Typeface.createFromAsset(getActivity().getAssets(), druidFontPath);
-        Typeface tfInterests = Typeface.createFromAsset(getActivity().getAssets(), interestsFontPath);
-        Typeface tfZodiac = Typeface.createFromAsset(getActivity().getAssets(), zodiacFontPath);
+        Typeface tfMain = Typeface.createFromAsset(getActivity().getAssets(), TITLE_FONT_PATH);
+        Typeface tfText = Typeface.createFromAsset(getActivity().getAssets(), TEXT_FONT_PATH);
+        Typeface tfDruid = Typeface.createFromAsset(getActivity().getAssets(), DRUID_FONT_PATH);
+        Typeface tfInterests = Typeface.createFromAsset(getActivity().getAssets(), INTERESTS_FONT_PATH );
+        Typeface tfZodiac = Typeface.createFromAsset(getActivity().getAssets(), ZODIAC_FONT_PATH);
+
         // Applying font
         txtGhost.setTypeface(tfMain);
         txtBoilerPlate.setTypeface(tfText);
@@ -87,9 +91,9 @@ public class MainFragment extends Fragment {
 
                     // Commit the transaction
                     fragmentTransaction.add(R.id.container, druid);
-                    Log.v(LOG_TAG, "*********put the Druid fragment on screen");
+                    Log.d(LOG_TAG, "put the Druid fragment on screen");
                     fragmentTransaction.commit();
-                    Log.v(LOG_TAG, "********committing the Druid fragment");
+                    Log.d(LOG_TAG, "committing the Druid fragment");
 
                 }
             }
@@ -106,9 +110,9 @@ public class MainFragment extends Fragment {
 
                     // Commit the transaction
                     fragmentTransaction.add(R.id.container, interests);
-                    Log.v(LOG_TAG, "put the Interests fragment on screen");
+                    Log.d(LOG_TAG, "put the Interests fragment on screen");
                     fragmentTransaction.commit();
-                    Log.v(LOG_TAG, "committing the Interests fragment");
+                    Log.d(LOG_TAG, "committing the Interests fragment");
 
                 }
             }
