f1ac5a22bae82f77a95590e4afbe91f8aa08fd0f
Planxty-Dan <dan.newport@detroitlabs.com>
2014-11-11 11:04:56 -0500

    fixed small clean code issues in ResultsActivity

--- app/src/main/java/com/detroitlabs/teamdruid/project_gift/Activities/ResultsActivity.java
+++ app/src/main/java/com/detroitlabs/teamdruid/project_gift/Activities/ResultsActivity.java
@@ -14,10 +14,6 @@ import com.detroitlabs.teamdruid.project_gift.R;
  */
 public class ResultsActivity extends Activity {
 
-    //call get intent.getextras
-//    use the bundle from get extras to set the arguments of the fragment that you're starting'
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,24 +30,4 @@ public class ResultsActivity extends Activity {
         }
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-
-    }
 }
