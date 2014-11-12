25e293d09f3c76e089cc49014926f7fa46cbce8a
elyselturner <elyse.turner@detroitlabs.com>
2014-11-12 15:31:36 -0500

    I added more stuff... not totally finished yet

--- app/src/main/java/com/detroitlabs/teamdruid/project_gift/Fragments/MainFragment.java
+++ app/src/main/java/com/detroitlabs/teamdruid/project_gift/Fragments/MainFragment.java
@@ -2,22 +2,45 @@ package com.detroitlabs.teamdruid.project_gift.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.detroitlabs.teamdruid.project_gift.R;
+import com.detroitlabs.teamdruid.project_gift.activities.ViewPagerAdapterActivity;
 
 /**
  * Created by elyseturner on 11/12/14.
  */
 public class MainFragment extends Fragment{
+    private ViewPager mPager;
+
+    /**
+     * The pager adapter, which provides the pages to the view pager widget.
+     */
+    private PagerAdapter mPagerAdapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_main,container, false);
-
         //make fragment state pager adapter subclass? here is also where we populate the data set... animals.. arrays... models
         //set that adapter on view pager
+
+        return inflater.inflate(R.layout.fragment_main,container, false);
+
+    }
+
+
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mPager = (ViewPager) view.findViewById(R.id.pager);
+//        mPagerAdapter = new ViewPagerAdapterActivity(getFragmentManager());
+        mPager.setAdapter(new ViewPagerAdapterActivity(getFragmentManager()));
+
     }
 }
+
