cc5da7dc66954396e5688510abd277902390ed35
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-11-13 13:39:35 -0500

    Making getters and setters for AnimalModel

--- app/src/main/java/com/detroitlabs/teamdruid/project_gift/Models/AnimalModel.java
+++ app/src/main/java/com/detroitlabs/teamdruid/project_gift/Models/AnimalModel.java
@@ -5,4 +5,22 @@ package com.detroitlabs.teamdruid.project_gift.models;
  */
 public class AnimalModel {
 
+    private String animalName;
+    private String animalSearchTerms;
+
+    public String getAnimalName() {
+        return animalName;
+    }
+
+    public void setAnimalName(String animalName) {
+        this.animalName = animalName;
+    }
+
+    public String getAnimalSearchTerms() {
+        return animalSearchTerms;
+    }
+
+    public void setAnimalSearchTerms(String animalSearchTerms) {
+        this.animalSearchTerms = animalSearchTerms;
+    }
 }
